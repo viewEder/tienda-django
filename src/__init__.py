42ddbc6980defe7f7dde631dab9370052f130009
Eder Lara T <ederlatru@gmail.com>
2022-09-22 08:14:17 -0500

    Configuración de base de datos y .env

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+# Importamos las librerias necesarias:
+import pymysql                      # Importaos el driver de base de datos
+from dotenv import load_dotenv
+
+# Cargar los métodos de las librerias:
+pymysql.install_as_MySQLdb()
+load_dotenv()
+
+
--- src/settings.py
+++ src/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vbw_*sdcjtf(p!&n*7zt=(li%pgvb025525#i1$fd5__+w^6p*'
+SECRET_KEY = os.getenv('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DBNAME'),
+        'USER': os.getenv('DBUSER'),
+        'HOST': os.getenv('DBHOST'),
+        'PASSWORD': os.getenv('DBPASS'),
+        'PORT': os.getenv('DBPORT'),
     }
 }
 
